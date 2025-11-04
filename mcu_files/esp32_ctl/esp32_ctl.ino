278c081154cd9e61415e3d992eb4271d70d47b00
Karl Calden <kcalden323@gmail.com>
2025-11-03 20:20:04 -0500

    Add pin definitions

--- mcu_files/esp32_ctl/esp32_ctl.ino
+++ mcu_files/esp32_ctl/esp32_ctl.ino
@@ -1,22 +1,35 @@
 #include <SPI.h>
 
+// Pin definitions
 #define DAC_VREF 4.98
-#define SYNC_PIN 5  // GPIO5 (IO5) → DAC_SYNC
-#define SCK_PIN 36  // GPIO36 (SCK) → DAC_SCLK
-#define SDO_PIN 35  // GPIO35 (SDO) → DAC_DIN
+#define SYNC_PIN 5            // GPIO5 (IO5) → DAC_SYNC
+#define SCK_PIN 36            // GPIO36 (SCK) → DAC_SCLK
+#define SDO_PIN 35            // GPIO35 (SDO) → DAC_DIN
+#define SER_CLK_WRITE_PIN 9  // A11 GPIO12
+#define SER_CLK_WRITE_EN_PIN 3 // P23 on board
+#define SER_MON_EN_PIN 38
+#define SER_DATA_MON_PIN 8
+#define SER_DATA_WRITE_PIN 33
+#define SER_CLK_MON_PIN 1
+#define RESET_WRITE_PIN 10
+#define LS_CH2_EN_PIN 7
+#define COMM_EN_PIN 18
+#define LS_CH2_DIR_PIN 14
+#define LS_CH1_DIR_PIN 12
+#define LS_CH1_EN_PIN 6
 
 // DAC CMDs
-#define CMD_WRITE_UPDATE 0x03 // Write to and update DAC
+#define CMD_WRITE_UPDATE 0x03  // Write to and update DAC
 
 // DAC Registers
-#define DAC_A   0x00
-#define DAC_B   0x01
-#define DAC_C   0x02
-#define DAC_D   0x03
-#define DAC_E   0x04
-#define DAC_F   0x05
-#define DAC_G   0x06
-#define DAC_H   0x07
+#define DAC_A 0x00
+#define DAC_B 0x01
+#define DAC_C 0x02
+#define DAC_D 0x03
+#define DAC_E 0x04
+#define DAC_F 0x05
+#define DAC_G 0x06
+#define DAC_H 0x07
 #define DAC_ALL 0x08
 
 // DAC Channel
@@ -29,16 +42,28 @@
 #define DAC_CH_V3P3 DAC_G
 #define DAC_CH_VECOM DAC_H
 
+// Global constants
 SPISettings spi_conf(2000000, MSBFIRST, SPI_MODE1);
+hw_timer_t* timer = NULL;  // Instance of a hardware timer
+volatile uint32_t clk_count = 200;
+volatile uint8_t clk_pin_state = 0;
 
 void configure_spi();
 void write_dac(uint8_t cmd, uint8_t address, uint16_t data);
 uint16_t voltageToCode(float voltage);
 void set_dac_voltage(uint8_t channel, float voltage);
 
+void ARDUINO_ISR_ATTR isr_func() {
+  digitalWrite(SER_CLK_WRITE_PIN, clk_pin_state);
+  clk_pin_state = !clk_pin_state;
+  clk_count--;
+}
+
 void setup() {
   configure_spi();
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(SER_CLK_WRITE_PIN, OUTPUT);
+  digitalWrite(SER_CLK_WRITE_PIN, 0);
   digitalWrite(LED_BUILTIN, HIGH);
 
   // Setup all of the bypasses
@@ -50,11 +75,18 @@ void setup() {
   set_dac_voltage(DAC_CH_VECOM, 1.65);
   set_dac_voltage(DAC_CH_VPWR, 3.55);
   set_dac_voltage(DAC_CH_VSUP, 3.55);
-}
-float dac_vout = 0.0;
-uint8_t dac_channel = 0;
-void loop() {
 
+
+
+  // timer = timerBegin(100000); //timer 1Mhz resolution
+  // timerAttachInterrupt(timer, &isr_func);
+  // timerAlarm(timer, 1, true, clk_count);
+}
+
+void loop() {
+  for (int i = 0; i < 100; i++) {
+    // digitalWrite(SER_CLK_WRITE_PIN, clk_pin_state)
+  }
 }
 
 void configure_spi() {
@@ -63,13 +95,10 @@ void configure_spi() {
 }
 
 void write_dac(uint8_t cmd, uint8_t address, uint16_t data) {
-  uint16_t addr16 = ((uint16_t) address) << 12;
+  uint16_t addr16 = ((uint16_t)address) << 12;
   SPI.beginTransaction(spi_conf);
   SPI.write32(
-    ((uint32_t)(0x0F & cmd) << 24) |
-    ((uint32_t)(addr16 | (data & 0x0FFF)) << 8) |
-    (uint32_t)0
-  );
+    ((uint32_t)(0x0F & cmd) << 24) | ((uint32_t)(addr16 | (data & 0x0FFF)) << 8) | (uint32_t)0);
   SPI.endTransaction();
   delayMicroseconds(32);
 }
